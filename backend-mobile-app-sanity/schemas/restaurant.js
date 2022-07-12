9da88715c234ee3d2f83ed1982ddcd4fecf2a344
Abdenour Touat <61945965+abdenour-progdes@users.noreply.github.com>
2022-07-12 18:35:56 +0100

    creating the schemas

---
+++ backend-mobile-app-sanity/schemas/restaurant.js
@@ -0,0 +1,63 @@
+export default {
+  name: "restaurant",
+  title: "Restaurant",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Restaurant name",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "short_description",
+      type: "string",
+      title: "Short description",
+      validation: (Rule) => Rule.max(200),
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Image of the Restaurant",
+    },
+    {
+      name: "lat",
+      type: "number",
+      description: "Latitude of the Restaurant",
+    },
+    {
+      name: "long",
+      type: "number",
+      title: "Longtitude of the Restaurant",
+    },
+    {
+      name: "address",
+      type: "string",
+      title: "Restaurant Address",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "rating",
+      type: "number",
+      title: "Enter a rating from 1 to 5",
+      validation: (Rule) =>
+        Rule.required()
+          .min(1)
+          .max(5)
+          .error("please enter a value between 1 and 5"),
+    },
+    {
+      name: "type",
+      title: "Category",
+      validation: (Rule) => Rule.required(),
+      type: "reference",
+      to: [{ type: "category" }],
+    },
+    {
+      name: "dishes",
+      type: "array",
+      title: "Dishes",
+      of: [{ type: "reference", to: [{ type: "dish" }] }],
+    },
+  ],
+};
